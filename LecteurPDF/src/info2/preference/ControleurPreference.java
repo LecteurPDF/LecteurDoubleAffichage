209e1ad4f5686d02a0ba42592c6626cfd3a84014
bipbip18 <44928784+bipbip18@users.noreply.github.com>
2019-01-30 10:32:05 +0100

    Ajout - interface préférences bouton

--- LecteurPDF/src/info2/preference/ControleurPreference.java
+++ LecteurPDF/src/info2/preference/ControleurPreference.java
@@ -2,50 +2,49 @@ package info2.preference;
 
 import java.io.IOException;
 
-import info2.lecteurpdf.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
 
-/**
- * Permet de controler les différents objets de SceneBuilder -> fichier "preference.fxml"
- * @author sannac, vivier, pouzelgues, renoleau
- * @version 1.0
- */
 public class ControleurPreference {
 
-    /** Menu de Gauche -> Affiche les différentes catégories du menu préférence */
     @FXML
     private ScrollPane affichageInfos;
 
-    /**
-     * Affiche sur la partie de droite les informations liées ( fichier fxml  'touches.fxml')
-     *   à la catégorie 'Touches'
-     * @param event inutilisé
-     */
     @FXML
     void optionsTouches(ActionEvent event) {
-        try {
-            affichageInfos.setContent(FXMLLoader.load(getClass().getResource("touches.fxml")));
-        } catch (IOException e) {
-            Main.journaux.warning("Problème lors de l'accés au fichier 'touches.fxml'");
-        }
+    	try {
+    		affichageInfos.setContent(FXMLLoader.load(getClass().getResource("touches.fxml")));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
-    /**
-     * Affiche sur la partie de droite les informations liées ( fichier fxml 'disposition.fxml' )
-     *   à la catégorie 'Disposition'
-     * @param event inutilisé
-     */
     @FXML
     void optionDisposition(ActionEvent event) {
-        try {
-            affichageInfos.setContent(FXMLLoader.load(getClass().getResource("disposition.fxml")));
-        } catch (IOException e) {
-            Main.journaux.warning("Problème lors de l'accés au fichier 'disposition.fxml'");
-        }
+    	try {
+    		affichageInfos.setContent(FXMLLoader.load(getClass().getResource("disposition.fxml")));
 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+
+    @FXML
+    void survolEntreeBouton(MouseEvent event) {
+    	((Button)event.getSource()).setStyle("-fx-background-color:#f17d49;-fx-text-fill:white;");
+    }
+
+    @FXML
+    void survolSortieBouton(MouseEvent event) {
+    	((Button)event.getSource()).setStyle("-fx-background-color:#e16731;-fx-text-fill:white;");
     }
 }
