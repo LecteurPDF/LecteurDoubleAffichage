2661b7b29d935fbd00f9a66454ccc3b11e39397b
pixalex <36847035+pixalex@users.noreply.github.com>
2019-02-04 15:08:15 +0100

    Ajout - Surpression d'une vue grace au menu contexuel

--- LecteurPDF/src/info2/lecteurpdf/ControleurVue.java
+++ LecteurPDF/src/info2/lecteurpdf/ControleurVue.java
@@ -10,14 +10,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextField;
+import javafx.scene.control.skin.SplitPaneSkin;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -207,6 +211,14 @@ public class ControleurVue implements Initializable {
 
 		try {
 			vue.getPdf().close();
+
+			Parent parent = vboxVue.getParent();
+			// Recueration du splitpane parent, celui qui contient les deux vue
+			SplitPane splitPaneParent = (SplitPane) parent.getParent().getParent();
+			//Retrait de l'anchorpane ou est la vue actuelle
+			splitPaneParent.getItems().remove(parent);
+
+
 		} catch( NullPointerException e) {
 
 		}
@@ -257,7 +269,7 @@ public class ControleurVue implements Initializable {
 
 			public void handle(ActionEvent event) {
 				// TODO : suppression
-				//fermetureVue(); -> Ne marche pas
+				fermetureVue();
 			}
 		});
 
