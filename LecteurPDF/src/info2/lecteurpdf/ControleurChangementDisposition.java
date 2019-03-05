95fea33930b885625b73a020034b4776075c9218
Alexis <alexis.vivier@iut-rodez.fr>
2019-03-06 00:07:51 +0100

    ControleurChangementDisposition - Mise çà jour fenetre Pop-Up

--- LecteurPDF/src/info2/lecteurpdf/ControleurChangementDisposition.java
+++ LecteurPDF/src/info2/lecteurpdf/ControleurChangementDisposition.java
@@ -4,21 +4,21 @@ import java.net.URL;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
-import info2.util.OutilLecture.PageInexistante;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Labeled;
 import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * Expéditeur : Celui qui va envoie la vue vers un nouvel AnchorPane
+ * Destinataire : Celui qui recevra la nouvelle vue
+ * @author Alexis
+ */
 public class ControleurChangementDisposition implements Initializable{
-
-	private LinkedList<Vue> listeTemp;
 
 	@FXML
 	private AnchorPane posA;
@@ -35,13 +35,33 @@ public class ControleurChangementDisposition implements Initializable{
 	@FXML
 	private AnchorPane supprimer;
 
+	/* Le label que l'on échange(drop) avec le drag */
+	private Label buffer;
+
+	/* La fenêtre que l'on bouge actuelelment */
+	private Emplacement emplacementTmp;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		LinkedList<Vue> liste = Vue.getListeVues();
+		AnchorPane position;
 
-		listeTemp = Vue.getListeVues();
+		for(Vue vue: liste) {
+			Label nomFich = new Label(vue.getPdf().getCheminFichier());
+			nomFich.setWrapText(true);
 
-		reloadDaD();
+			position = determinePosition(vue.getEmplacement());
+
+			// On positionne et ajoute la vue dans l'AnchorPane
+			AnchorPane.setTopAnchor(nomFich, 0.0);
+			AnchorPane.setLeftAnchor(nomFich, 0.0);
+			AnchorPane.setRightAnchor(nomFich, 0.0);
+			AnchorPane.setBottomAnchor(nomFich, 0.0);
+			position.getChildren().add(nomFich);
+
+			dragDetected(nomFich);
+
+		}
 
 		AnchorPane[] tabAnchor = {
 				posA,
@@ -50,121 +70,166 @@ public class ControleurChangementDisposition implements Initializable{
 				posD
 		};
 
-		/* Zones de drop */
 		for(AnchorPane anchor:tabAnchor) {
+			dragOver(anchor);
 
-			/**
-			 * Actions lors du passage de la zone de drop
-			 */
-			anchor.setOnDragOver(dragEvent -> {
-				final Dragboard dragBroard = dragEvent.getDragboard();
+			dragDropped(anchor);
 
-				if (dragEvent.getGestureSource() != anchor && dragBroard.hasString()) {
-					// Indique les modes de transfert autorisés sur cette destination.
-					dragEvent.acceptTransferModes(TransferMode.MOVE);
-				}
-				dragEvent.consume();
-
-			});
-
-			/**
-			 * Action lors du drop dans la zone
-			 */
-			anchor.setOnDragDropped(dragEvent -> {
-				final Dragboard dragBroard = dragEvent.getDragboard();
-				boolean success = false;
-				if (dragBroard.hasString() ) {
-
-					Label nomfich = new Label(dragBroard.getString());
-					nomfich.setWrapText(true);
-
-					System.out.println(dragBroard.getContent(DataFormat.lookupMimeType(Vue.class.getName())));
-
-					// On positionne et ajoute la vue dans l'AnchorPane
-					AnchorPane.setTopAnchor(nomfich, 0.0);
-					AnchorPane.setLeftAnchor(nomfich, 0.0);
-					AnchorPane.setRightAnchor(nomfich, 0.0);
-					AnchorPane.setBottomAnchor(nomfich, 0.0);
-					anchor.getChildren().add(nomfich);
-
-					success = true;
-				}
-				/* let the source know whether the string was successfully
-				 * transferred and used */
-				dragEvent.setDropCompleted(success);
-
-				dragEvent.consume();
-
-				reloadDaD();
-
-			});
 		}
-
-
 
 	}
 
-	/**
-	 * Determine la position dans les cadres de l'interface
-	 * @param vue vue à determiner sa position
-	 * @return AnchorPane du cadre concerné par la vue
-	 */
-	private AnchorPane determinePosition(Vue vue) {
-		Emplacement emplacement = vue.getEmplacement();
+	private AnchorPane determinePosition(Emplacement emplacement) {
 
 		AnchorPane[][] tabPositions = {
 				{posA,posB},
 				{posC,posD},
 		};
-
+		System.out.println(tabPositions[emplacement.getFenetre()-1][emplacement.getPosition()-1]);
 		return tabPositions[emplacement.getFenetre()-1][emplacement.getPosition()-1];
 
 	}
 
-	private void reloadDaD() {
-		AnchorPane position;
+	private void dragDetected(Label nomFich) {
+		nomFich.setOnDragDetected(mouseEvent -> {
+			System.out.println("DnD detecté.");
+			final Dragboard dragBroard = nomFich.startDragAndDrop(TransferMode.MOVE);
+			// Remlissage du contenu.
+			ClipboardContent content = new ClipboardContent();
+			content.putString(nomFich.getText());
+			dragBroard.setContent(content);
+			LinkedList<Vue> liste = Vue.getListeVues();
 
-		/* Pour chaques vues les mettre dans le bon cadre */
-		for(Vue vue: listeTemp) {
-
-			Label nomfich = new Label(vue.getPdf().getCheminFichier());
-			nomfich.setWrapText(true);
-
-			position = determinePosition(vue);
-
-			// On positionne et ajoute la vue dans l'AnchorPane
-			AnchorPane.setTopAnchor(nomfich, 0.0);
-			AnchorPane.setLeftAnchor(nomfich, 0.0);
-			AnchorPane.setRightAnchor(nomfich, 0.0);
-			AnchorPane.setBottomAnchor(nomfich, 0.0);
-			position.getChildren().add(nomfich);
-
-			/**
-			 * Detection du drag
-			 */
-			nomfich.setOnDragDetected(mouseEvent -> {
-				System.out.println("DnD detecté.");
-				final Dragboard dragBroard = nomfich.startDragAndDrop(TransferMode.MOVE);
-				// Remlissage du contenu.
-				ClipboardContent content = new ClipboardContent();
-
-				try {
-					Image image = vue.getPdf().getPagePdfToImg(0).getImage();
-
-					content.putString(vue.getPdf().getCheminFichier());
-					content.putImage(image);
-					content.put(DataFormat.lookupMimeType(Vue.class.getName()), vue);
-				} catch (PageInexistante e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			/* On récupéère l'emplacement de la vue que l'on déplace */
+			for(Vue vue: liste) {
+				if(vue.getPdf().getCheminFichier().equals(nomFich.getText())) {
+					emplacementTmp = vue.getEmplacement();
 				}
-				dragBroard.setContent(content);
+			}
 
-				mouseEvent.consume();
-			});
+			mouseEvent.consume();
+		});
 	}
-}
 
+
+	private void dragOver(AnchorPane anchor) {
+		anchor.setOnDragOver(dragEvent -> {
+			final Dragboard dragBroard = dragEvent.getDragboard();
+
+			if (dragEvent.getGestureSource() != anchor && dragBroard.hasString()) {
+				// Indique les modes de transfert autorisés sur cette destination.
+				dragEvent.acceptTransferModes(TransferMode.MOVE);
+			}
+			dragEvent.consume();
+
+		});
+	}
+
+
+	private void dragDropped(AnchorPane anchor) {
+		anchor.setOnDragDropped(dragEvent -> {
+			final Dragboard dragBroard = dragEvent.getDragboard();
+			boolean success = false;
+			if (dragBroard.hasString()) {
+
+				Label nomFich = new Label(dragBroard.getString()); // Label qui contiendra l'expediteur
+				nomFich.setWrapText(true);
+
+				/* On prépare à la réception de l'expéditeur */
+
+
+				// TODO PB ICI
+				AnchorPane expediteur = determinePosition(emplacementTmp); // On récupère l'emplacement de l'expediteur
+																		   // afin de l'échanger ( swap ) avec le destinataire.
+
+				/* On retire le label destintaire du destinataire si il existe */
+				if (expediteur.getChildren().size() > 0) {
+					expediteur.getChildren().remove(0);
+				}
+
+				/* On place le label destinataire dans l'expéditeur */
+				Label nomFich2 = new Label();
+				if (anchor.getChildren().size() > 0) {
+					nomFich2 = (Label)anchor.getChildren().get(0);
+					AnchorPane.setTopAnchor(nomFich2, 0.0);
+					AnchorPane.setLeftAnchor(nomFich2, 0.0);
+					AnchorPane.setRightAnchor(nomFich2, 0.0);
+					AnchorPane.setBottomAnchor(nomFich2, 0.0);
+					expediteur.getChildren().add(nomFich2);
+				}
+
+				dragDetected(nomFich2); // Mise à jour de l'événement de déplacement
+
+				/* On place le label de l'expéditeur dans le destinaire */
+				AnchorPane.setTopAnchor(nomFich, 0.0);
+				AnchorPane.setLeftAnchor(nomFich, 0.0);
+				AnchorPane.setRightAnchor(nomFich, 0.0);
+				AnchorPane.setBottomAnchor(nomFich, 0.0);
+				anchor.getChildren().add(nomFich);
+
+				/* On recharge les différents événements sur la page */
+				dragDetected(nomFich); // Mise à jour de l'événement de déplacement
+
+				/* On échange les vues dans la liste */
+				LinkedList<Vue> liste = Vue.getListeVues();
+				int indexExpediteur = -1;
+				int indexDestinataire =  -1;
+
+				/* On récupéère l'emplacement de la vue que l'on déplace */
+				for(int i = 0 ; i < liste.size() ; i++) {
+					if(liste.get(i).getPdf().getCheminFichier().equals(nomFich.getText())) {
+						indexExpediteur =  i;
+					}
+					if(liste.get(i).getPdf().getCheminFichier().equals(nomFich2.getText())) {
+						indexDestinataire =  i;
+					}
+				}
+
+				System.out.println(indexExpediteur);
+				System.out.println(indexDestinataire);
+
+				/* On échange les emplacements */
+
+				if (indexDestinataire != -1) {
+					liste.get(indexExpediteur).setEmplacement(liste.get(indexDestinataire).getEmplacement()); // On place l'expediteur dans le destinataire
+
+				} else {
+					try {
+						Emplacement emplacementDestinataire;
+						if (expediteur.equals(posA)) {
+							emplacementDestinataire = new Emplacement(0,0);
+						} else if (expediteur.equals(posB)) {
+							emplacementDestinataire = new Emplacement(0,1);
+						} else if (expediteur.equals(posC)) {
+							emplacementDestinataire = new Emplacement(1,0);
+						} else {
+							emplacementDestinataire = new Emplacement(1,1);
+						}
+
+						liste.get(indexExpediteur).setEmplacement(emplacementDestinataire);
+					} catch (Exception e) {
+						System.out.println("Erreur avec emplacement");
+					}
+				}
+
+				if (nomFich2.getText().length() > 0) { // Si le destinataire est une vue existante, on le place dans l'expéditeur
+					liste.get(indexDestinataire).setEmplacement(liste.get(indexExpediteur).getEmplacement());
+				}
+				success = true;
+
+			}
+			/* let the source know whether the string was successfully
+			 * transferred and used  TODO Français */
+			dragEvent.setDropCompleted(success);
+
+			dragEvent.consume();
+		});
+	}
+
+
+	private void reloadDaD(Label nomFich) {
+
+	}
 
 
 }
