c60c69ccd76f3d839364ae0280f85445f4cc9281
audricPouzelgues <audric.pouzelgues@iut-rodez.fr>
2019-03-22 01:15:53 +0100

    Modification de l'interface des prédéfinies

--- src/main/java/visioreader/preference/ControleurDisposition.java
+++ src/main/java/visioreader/preference/ControleurDisposition.java
@@ -1,12 +1,21 @@
 package visioreader.preference;
 
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import visioreader.util.Emplacement;
+import visioreader.util.EmplacementIncorrect;
+import visioreader.util.Preferences;
+import visioreader.vue.Vue;
 
 /**
  * Controleur qui permet de gérer la fenêtre de dispostion présente dans le menu préférence
@@ -15,107 +24,33 @@ import javafx.scene.input.MouseEvent;
  * @author sannac, vivier, pouzelgues, renoleau
  * @version 1.0
  */
-public class ControleurDisposition {
+public class ControleurDisposition implements Initializable {
 
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Label lab1DG;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Label lab2DG;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Button btn_inverserDG;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Button btn_inverserHB;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Label lab1HB;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private Label lab2HB;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private RadioButton radDG;
-
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
-    @FXML
-    private RadioButton radHB;
-
-    /** Valide les changements de disposition */
     @FXML
     private Button btn_valider;
 
-    /** Annule les changements de disposition */
     @FXML
     private Button btn_annuler;
 
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
     @FXML
-    private RadioButton horizontal;
+    private RadioButton btn11;
 
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
     @FXML
     private ToggleGroup choix;
 
-    /** TODO commenter le rôle de ce champ (attribut, rôle d'association) */
     @FXML
-    private RadioButton vertical;
+    private RadioButton btn121;
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		btn11.setSelected(true);
 
+	}
 
-    /**
-     * Inverse les champs contenus dans les labels
-     * lab1DG
-     * et
-     * lab2DG
-     * @param event
-     */
-    @FXML
-    void inversionDG(ActionEvent event) {
-    	String temp = lab1DG.getText();
-    	lab1DG.setText(lab2DG.getText());
-    	lab2DG.setText(temp);
-    }
-
-    /**
-     * Inverse les champs contenus dans les labels
-     * lab1HB
-     * et
-     * lab2HB
-     * @param event
-     */
-    @FXML
-    void inversionHB(ActionEvent event) {
-    	String temp = lab1HB.getText();
-    	lab1HB.setText(lab2HB.getText());
-    	lab2HB.setText(temp);
-    }
-
-    /**
-     * TODO commenter le rôle de cette méthode
-     * @param event
-     */
-    @FXML
-    void inversion(MouseEvent event) {
+	@FXML
+    void changements(ActionEvent event) {
+		//System.out.println("coucou");
 
     }
-
-    /**
-     * TODO commenter le rôle de cette méthode
-     * @param event
-     */
-    @FXML
-    void valider(ActionEvent event) {
-
-    }
-
 
 }
